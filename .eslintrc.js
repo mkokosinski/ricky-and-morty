e42975f8309a87ac8735cb95ca810ba51fcfdd50
m.kokosinski <m.kokosinski@mymusic.pl>
2021-10-16 19:21:29 +0200

    base project config

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'airbnb-typescript'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    project: ['./tsconfig.json'],
+    sourceType: 'module',
+  },
+  ignorePatterns: ["babel.config.js"],
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    "linebreak-style": 0,
+    'no-console': 1,
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [1, { extensions: ['.tsx', '.ts'] }],
+    'react/require-default-props': 0
+  },
+};
